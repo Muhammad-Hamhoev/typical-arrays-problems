3316f92240f51c96f4702317a5b9907e588d873c
Maga <ded.hamhoev@gmail.com>
2021-03-07 21:38:47 +0300

    done task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    if (array == 0 || array == undefined) {
+        return 0;
+    } else {
+        return +array.sort((a, b) => {
+            return a - b;
+        })[0];
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+    if (array == 0 || array == undefined) {
+        return 0;
+    } else {
+        return +array.sort((a, b) => {
+            return a - b;
+        })[array.length - 1];
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    if (array == 0 || array == undefined) {
+        return 0;
+    } else {
+        let num = 0;
+
+        array.forEach(item => {
+            return num += item;
+        });
+
+        return num / 20
+    }
 }
